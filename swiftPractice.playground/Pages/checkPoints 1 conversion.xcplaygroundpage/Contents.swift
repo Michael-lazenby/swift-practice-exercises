eb65844745b5ff9304668dc39f54f873689ddd73
michael Lazenby <michaelazenby@gmail.com>
2023-01-27 22:45:53 -0800

    playground work

---
+++ swiftPractice.playground/Pages/checkPoints 1 conversion.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let celcius = 45.0
+var fahrenheit = Double((celcius * 9)/5)
+print("the temperature in cel is \(celcius) and in \(fahrenheit)")
+
+//: [Next](@next)
---
+++ swiftPractice.playground/Pages/checkpoint 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+//: [Previous](@previous)
+
+import Foundation
+let names = ["michael", "smith", "suriya", "michael"]
+print(names)
+print(names.count)
+let uniqueArray = Array(Set(names))
+
+//: [Next](@next)
